69a89bdd4fed7ba7ea3493026dfdc992ad325ffb
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-29 14:22:11 +0200

    install redux-toolkit

--- src/store.js
+++ src/store.js
@@ -1,11 +1,20 @@
 import { createStore } from "redux";
 import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { loggerMiddleware } from "./addons/middleware";
-import thunkMiddldeware from "redux-thunk";
-import { reducer } from "./reducer";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import thunkMiddldeware from "redux-thunk";
+// import { reducer } from "./reducer";
 
-export const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunkMiddldeware))
-);
+// export const store = createStore(
+//   reducer,
+//   composeWithDevTools(applyMiddleware(thunkMiddldeware))
+// );
+import { configureStore } from "@reduxjs/toolkit";
+import { todosReducer } from "./components/todos/todosReducer";
+import { filtersReducer } from "./components/filters/filtersReducer";
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+});
