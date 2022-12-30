4b1b462d23880a800191d2a30d96d7cec36d9c6c
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-30 08:54:40 +0200

    install tailwindcss

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,5 @@
-console.log('we are back again.');
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <div className="app">hello I am back again.</div>
+);
