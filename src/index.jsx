2c18a9f7815e3d86bf907d82688d268a47b8471e
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-30 08:47:57 +0200

    cleaning up

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { Application } from "./Application";
-
-import "./api/server";
-import { store } from "./store";
-import { fetchTodos } from "./components/todos/todosReducer";
-
-store.dispatch(fetchTodos);
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Application />
-    </Provider>
-  </React.StrictMode>
-);
+console.log('we are back again.');
--- src/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { todosReducer } from "./components/todos/todosReducer";
-import { filtersReducer } from "./components/filters/filtersReducer";
-
-export const reducer = combineReducers({
-  todos: todosReducer,
-  filters: filtersReducer,
-});
