a4b8696ed606f4831d7c9dbeaeee9612aea952eb
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-30 08:24:11 +0200

    wip

--- src/addons/middleware.js
+++
@@ -1,8 +0,0 @@
-export function loggerMiddleware(store) {
-  return function dispatch(next) {
-    return function handleAction(action) {
-      //      console.log("dispatch", action);
-      return next(action);
-    };
-  };
-}
--- src/store.js
+++ src/store.js
@@ -1,13 +1,3 @@
-import { createStore } from "redux";
-import { applyMiddleware } from "redux";
-// import { composeWithDevTools } from "@redux-devtools/extension";
-// import thunkMiddldeware from "redux-thunk";
-// import { reducer } from "./reducer";
-
-// export const store = createStore(
-//   reducer,
-//   composeWithDevTools(applyMiddleware(thunkMiddldeware))
-// );
 import { configureStore } from "@reduxjs/toolkit";
 import { todosReducer } from "./components/todos/todosReducer";
 import { filtersReducer } from "./components/filters/filtersReducer";
