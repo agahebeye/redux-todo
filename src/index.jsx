35703a01828f9d4947b6e4adf91b889805331931
Gahebeye aboubakar <Agahebeye@gmail.com>
2022-12-30 10:22:03 +0200

    testing enhancers

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,4 @@
 import { createRoot } from "react-dom/client";
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 import { store } from "./store";
-
-const subscriber = store.subscribe(() =>
-  console.log("state", store.getState().todos.items)
-);
-
-store.dispatch({ type: "todos/added", payload: "Wash dishes" });
-store.dispatch({ type: "todos/added", payload: "Meet my girlfriend" });
-
-subscriber();
-
-createRoot(document.getElementById("root")).render(
-  <div className="app">hello I am back again.</div>
-);
+createRoot(document.getElementById("root")).render(<div></div>);
--- src/store.js
+++ src/store.js
@@ -3,9 +3,9 @@ import { createStore, combineReducers } from "redux";
 import { todosReducer } from "./reducers/todosReducer";
 import { filtersReducer } from "./reducers/filtersReducer";
 
-export const store = createStore(
-  combineReducers({
-    todos: todosReducer,
-    filters: filtersReducer,
-  })
-);
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filtersReducer,
+});
+
+export const store = createStore(rootReducer);
